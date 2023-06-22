2613eff3f260b225eb9f22b9aedf49ac639f65d9
vladbevz <bevz.vlad15@gmail.com>
2023-06-22 21:23:08 +0200

    update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const background = document.querySelector("body");
+const textColor = document.querySelector(".color");
+const changeButton = document.querySelector(".change-color");
+
+changeButton.addEventListener("click", getColor);
+function getColor() {
+  let color = getRandomHexColor();
+  background.style.backgroundColor = color;
+  textColor.textContent = color;
+}
