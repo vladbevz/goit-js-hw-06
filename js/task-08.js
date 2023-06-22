65ee9d1d6b37b955b4c9df68048d91461e317607
vladbevz <bevz.vlad15@gmail.com>
2023-06-22 21:14:45 +0200

    update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", formSubmit);
+function formSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value == "" || password.value == "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+  event.currentTarget.reset();
+}
